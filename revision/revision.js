73af91d850b33b232540452a35d69fc94ac3ede0
CaseyMartin23 <cm232311@gmail.com>
2020-01-10 07:52:48 +0200

    yeeetgit statusgit status

--- revision/revision.js
+++ revision/revision.js
@@ -28,3 +28,51 @@ const multiplyArrayAndReturnPosOrNeg = (arrayOfNumbers) => {
     return total > 0 ? "Positive" : total < 0 ? "Negative" : "Zero";
 }
 
+class Student {
+    Name;
+    Marks;
+    constructor(name, marks){
+        this.Name = name;
+        this.Marks = marks;
+    }
+
+    set setName(studentName) {
+        return this.Name = studentName;
+    }
+    get getName() {
+        return this.Name;
+    }
+
+    set setMarks(studentMarks) {
+        return this.Marks = studentMarks;
+    }
+    get getMarks() {
+        return this.Marks;
+    }
+}
+
+let studentArr = [];
+
+studentArr.push(new Student("James", 75));
+studentArr.push(new Student("Rose", 80));
+studentArr.push(new Student("Steve", 67));
+studentArr.push(new Student("Jane", 89));
+studentArr.push(new Student("Coleen", 52));
+
+// console.log(studentArr);
+
+const stundentsNamesOrMarks = (arrayOfStudents, field = "Names") => {
+    let nameOrMarks = field.toLowerCase();
+    console.log("field ==> ", nameOrMarks)
+    let result = nameOrMarks == "names" ? arrayOfStudents.map(({Name}) => `${Name}`) 
+    : nameOrMarks == "marks" 
+    ? arrayOfStudents.map(({Marks}) => `${Marks}`) 
+    : arrayOfStudents.map(({Name, Marks}) => `${Name}, ${Marks}`)
+    console.log(result)
+
+    let res;
+    for(let i = 0; i < result.length; i++) result[i];
+
+}
+
+console.log(stundentsNamesOrMarks(studentArr))
